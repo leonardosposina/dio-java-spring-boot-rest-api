78adbebaa1df3eb022a56bd6b5b05c6435678cfe
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-01-13 17:08:11 -0300

    feat: Person and Phone entities add

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.personapi.entity;
+
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+  private Long id;
+  private PhoneType type;
+  private String number;
+}

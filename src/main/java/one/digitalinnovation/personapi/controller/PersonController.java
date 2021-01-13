4265dc30bbe7405379585886ccdf1eeb321be16d
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-01-13 18:39:03 -0300

    feat: PersonController createPerson add

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,15 +1,32 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("/api/v1/person")
 public class PersonController {
 
-  @GetMapping
-  public String getBook() {
-    return "API test!";
+  private PersonRepository personRepository;
+
+  @Autowired
+  public PersonController(PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
+  @PostMapping
+  public MessageResponseDTO createPerson(@RequestBody Person person) {
+    Person savedPerson = personRepository.save(person);
+
+    return MessageResponseDTO
+      .builder()
+      .message("Created with ID: " + savedPerson.getId())
+      .build();
   }
 }

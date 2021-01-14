70f1680a2a510dcace6e4882b9ee46e03ec8307e
Leonardo Sposina <leonardo.sposina@icloud.com>
2021-01-14 16:06:32 -0300

    fix: PersonDTO phone type fix

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import one.digitalinnovation.personapi.entity.Phone;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.time.LocalDate;
 import java.util.List;
 
 @Data
@@ -35,5 +33,5 @@ public class PersonDTO {
 
   @Valid
   @NotEmpty
-  private List<Phone> phones;
+  private List<PhoneDTO> phones;
 }
